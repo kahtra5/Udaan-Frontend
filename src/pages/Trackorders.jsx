9b427973b74bc098e33ada110417c31f8b008eb3
kahtra5 <94981153+kahtra5@users.noreply.github.com>
2024-12-31 22:12:31 +0530

    Update Trackorders.jsx

--- src/pages/Trackorders.jsx
+++ src/pages/Trackorders.jsx
@@ -24,7 +24,7 @@ ChartJS.register(
 
 
 
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 const TrackOrder = () => {
   const [restaurants, setRestaurants] = useState([]);
